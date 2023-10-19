a025fd6262b7a1a6d77d95f72d5a37febd41edd7
Nathaniel Kemme Nash <nkemme54@gmail.com>
2023-10-19 17:44:45 -0400

    Added collapsing file window

--- src/filesWindow/filesWindow.jsx
+++ src/filesWindow/filesWindow.jsx
@@ -2,28 +2,53 @@ import { useState, useEffect } from 'react'
 import './filesWindow.css'
 import FileTile from './Components/fileTile'
 import FilesTopBar from './topBar'
-
+import forwardIcon from '../assets/forward_icon.png'
+import backwardIcon from '../assets/backward_icon.png'
+import backIcon from '../assets/back_icon.png'
 function FilesWindow({handleSelectFile, files, addFile, projName, setProjName, exitProj, canCloudSave, cloudSave}){
+    const [mini, setMini] = useState(false)
     useEffect(() => {
         console.log('files changed')
         console.log(files)
     }
     , [files])
     
+    function handlieFlip(){
+        setMini(!mini)
+    }
+
+    async function handleBackButton(){
+        setMini(false)
+        await exitProj()
+    }
+     
     
     return (
-        <div className='FilesWindow'>
-            <FilesTopBar addFile={addFile} projName={projName} setProjName={setProjName} exitProj={exitProj} canCloudSave={canCloudSave} cloudSave={cloudSave}/>
-            <div className='Files'>
-                {files.map((file, index) => {
-                    return (
-                    <FileTile fileName={file} key={index} handleSelectFile={handleSelectFile}/>
-                    )
-                })}
+        <>
+            {mini ? 
+            <div className='FilesWindowMini'>
+                <img src={backIcon} className='BackIconMini' alt='Back' onClick={handleBackButton}/>
+                <div className='FilesBottom'>
+                    <img src={forwardIcon} alt={"minimize"} onClick={handlieFlip} className='MinimizeIcon'/>
+                </div>
             </div>
-            
-            
-        </div>
+            : <div className='FilesWindow'>
+                <FilesTopBar addFile={addFile} projName={projName} setProjName={setProjName} exitProj={exitProj} canCloudSave={canCloudSave} cloudSave={cloudSave}/>
+                <div className='Files'>
+                    {files.map((file, index) => {
+                        return (
+                        <FileTile fileName={file} key={index} handleSelectFile={handleSelectFile}/>
+                        )
+                    })}
+                </div>
+                <div className='FilesBottom'>
+                    {projName != "" ? <img src={backwardIcon} alt={"minimize"} onClick={handlieFlip} className='MinimizeIcon'/> : null}
+                </div>
+                
+                
+                
+            </div>}
+        </>
     )
 }
 
